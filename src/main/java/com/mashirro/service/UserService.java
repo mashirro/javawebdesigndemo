85728907f83dc19307910bfb68f94495261aa31c
chenzg <18627901759@163.com>
2020-08-13 11:55:42 +0800

    对开发人员隐藏Connection,为了确保一个线程中只有一个Connection，我们可以使用ThreadLocal来存放本地线程变量，可以将ThreadLocal理解为一个隔离线程的容器

--- src/main/java/com/mashirro/service/UserService.java
+++ src/main/java/com/mashirro/service/UserService.java
@@ -6,8 +6,6 @@ import com.mashirro.utils.DatabaseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,28 +14,39 @@ import java.util.List;
 public class UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
+//    public List<User> getUserList(User user) {
+//        Connection coon = null;
+//        List<User> userList = new ArrayList<>();
+//        try {
+//            String sql = "select * from user";
+//            coon = DatabaseUtil.getConnection();
+////            PreparedStatement ps = coon.prepareStatement(sql);
+////            ResultSet rs = ps.executeQuery();
+////            while (rs.next()) {
+////                User u = new User();
+////                u.setId(rs.getString("id"));
+////                u.setUserName(rs.getString("user_name"));
+////                u.setTelephone(rs.getString("telephone"));
+////                userList.add(u);
+////            }
+//            userList = DatabaseUtil.queryEntityList(coon, User.class, sql);
+//        } catch (Exception e) {
+//            logger.error("用户列表查询出现异常!", e);
+//            e.printStackTrace();
+//        } finally {
+//            DatabaseUtil.closeConnection(coon);
+//        }
+//        return userList;
+//    }
+
+
+    /**
+     * 获取用户列表
+     * @param user
+     * @return
+     */
     public List<User> getUserList(User user) {
-        Connection coon = null;
-        List<User> userList = new ArrayList<>();
-        try {
-            String sql = "select * from user";
-            coon = DatabaseUtil.getConnection();
-//            PreparedStatement ps = coon.prepareStatement(sql);
-//            ResultSet rs = ps.executeQuery();
-//            while (rs.next()) {
-//                User u = new User();
-//                u.setId(rs.getString("id"));
-//                u.setUserName(rs.getString("user_name"));
-//                u.setTelephone(rs.getString("telephone"));
-//                userList.add(u);
-//            }
-            userList = DatabaseUtil.queryEntityList(coon, User.class, sql);
-        } catch (Exception e) {
-            logger.error("用户列表查询出现异常!", e);
-            e.printStackTrace();
-        } finally {
-            DatabaseUtil.closeConnection(coon);
-        }
-        return userList;
+        String sql = "select * from user";
+        return DatabaseUtil.queryEntityList(User.class, sql);
     }
 }
--- src/main/java/com/mashirro/utils/DatabaseUtil.java
+++ src/main/java/com/mashirro/utils/DatabaseUtil.java
@@ -22,6 +22,7 @@ public class DatabaseUtil {
     private static final String username;
     private static final String password;
     private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+    private static final ThreadLocal<Connection> CONNECTION_HOLDER = new ThreadLocal<>();
 
 
     static {
@@ -40,24 +41,63 @@ public class DatabaseUtil {
     }
 
 
+    /**
+     * 获取数据库连接
+     *
+     * @return
+     */
     public static Connection getConnection() {
-        Connection coon = null;
-        try {
-            coon = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            logger.error("获取数据库连接出现异常!", e);
-            e.printStackTrace();
+//        Connection coon = null;
+//        try {
+//            coon = DriverManager.getConnection(url, username, password);
+//        } catch (SQLException e) {
+//            logger.error("获取数据库连接出现异常!", e);
+//            e.printStackTrace();
+//        }
+//        return coon;
+        Connection coon = CONNECTION_HOLDER.get();
+        if (coon == null) {
+            try {
+                coon = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                logger.error("获取数据库连接出现异常!", e);
+                e.printStackTrace();
+            } finally {
+                CONNECTION_HOLDER.set(coon);
+            }
         }
         return coon;
     }
 
-    public static void closeConnection(Connection connection) {
-        if (connection != null) {
+    /**
+     * 关闭数据库连接
+     *
+     * @param connection
+     */
+//    public static void closeConnection(Connection connection) {
+//        if (connection != null) {
+//            try {
+//                connection.close();
+//            } catch (SQLException e) {
+//                logger.error("关闭数据库连接出现异常!", e);
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    /**
+     * 关闭数据库连接
+     */
+    public static void closeConnection() {
+        Connection coon = CONNECTION_HOLDER.get();
+        if (coon != null) {
             try {
-                connection.close();
+                coon.close();
             } catch (SQLException e) {
                 logger.error("关闭数据库连接出现异常!", e);
                 e.printStackTrace();
+            } finally {
+                CONNECTION_HOLDER.remove();
             }
         }
     }
@@ -73,16 +113,40 @@ public class DatabaseUtil {
      * @param <T>         泛型
      * @return
      */
-    public static <T> List<T> queryEntityList(Connection coon, Class<T> entityClass, String sql, Object... params) {
+//    public static <T> List<T> queryEntityList(Connection coon, Class<T> entityClass, String sql, Object... params) {
+//        List<T> entityList = new ArrayList<>();
+//        try {
+//            entityList = QUERY_RUNNER.query(coon, sql, new BeanListHandler<T>(entityClass), params);
+//        } catch (SQLException e) {
+//            logger.error("查询实体列表出错!", e);
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        } finally {
+//            closeConnection(coon);
+//        }
+//        return entityList;
+//    }
+
+    /**
+     * 查询实体列表
+     *
+     * @param entityClass 实体Class对象
+     * @param sql         sql语句
+     * @param params      参数
+     * @param <T>         泛型
+     * @return
+     */
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
         List<T> entityList = new ArrayList<>();
         try {
+            Connection coon = getConnection();
             entityList = QUERY_RUNNER.query(coon, sql, new BeanListHandler<T>(entityClass), params);
         } catch (SQLException e) {
             logger.error("查询实体列表出错!", e);
             e.printStackTrace();
             throw new RuntimeException(e);
         } finally {
-            closeConnection(coon);
+            closeConnection();
         }
         return entityList;
     }
